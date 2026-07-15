8fda797a3c6d00fd3fabe80f62ff90b00135ae33
SoulFlameAdmin <soulflame.mitko@gmail.com>
2026-07-16 00:17:49 +0300

    fix: hide POI markers while drawing a parking polygon

--- v2-map-first.js
+++ v2-map-first.js
@@ -2,6 +2,7 @@
   'use strict';
   const app=window.SFV2,s=app.state;
   const selectDestination=app.selectDestination;
+  const setDrawingMode=app.setDrawingMode;
 
   app.selectDestination=item=>{
     app.abortRequest?.('nearbyParking');
@@ -12,5 +13,17 @@
     app.closeMapMenu?.();
     selectDestination(item);
     app.setSheetCollapsed(true);
+  };
+
+  app.setDrawingMode=active=>{
+    setDrawingMode(active);
+    if(!s.map)return;
+    if(active){
+      if(s.map.hasLayer(s.parkingLayer))s.map.removeLayer(s.parkingLayer);
+      if(s.map.hasLayer(s.fuelLayer))s.map.removeLayer(s.fuelLayer);
+    }else{
+      if(s.layers.parking&&!s.map.hasLayer(s.parkingLayer))s.parkingLayer.addTo(s.map);
+      if(s.layers.fuel&&!s.map.hasLayer(s.fuelLayer))s.fuelLayer.addTo(s.map);
+    }
   };
 })();
